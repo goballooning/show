53257fcd81a4130524e55dfe3dd2b30f0001b0af
ying17zi <lihongying17@gmail.com>
2015-05-17 00:14:05 +0800

    clean function args; add show_mesh, and saveMesh4Mayavi.m

---
+++ saveMesh4Mayavi.m
@@ -0,0 +1,11 @@
+function saveMesh4Mayavi(file_name, fv, colors)
+
+x = fv.vertices(:, 1);
+y = fv.vertices(:, 2);
+z = fv.vertices(:, 3);
+triangles = fv.faces;
+
+
+save(file_name, 'x', 'y', 'z', 'triangles', 'colors');
+
+end %
